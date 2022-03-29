25a3a5835f8baf751a4002b1020063583a16e048
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-29 16:29:00 -0600

    Feat: add add_dish to potluck

--- test/potluck_spec.rb
+++ test/potluck_spec.rb
@@ -17,5 +17,15 @@ describe Potluck do
     expect(potluck.dishes).to eq([])
   end
 
+  it "can add dishes to dishes" do
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+
+    expect(potluck.dishes).to eq([couscous_salad, cocktail_meatballs])
+  end
+
 
 end

802514c6b1097e27e1ed2d242b12b5b9dce232bd
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-29 16:22:46 -0600

    Feat: create potluck object

---
+++ lib/potluck.rb
@@ -0,0 +1,7 @@
+class Potluck
+
+  def initialize(date)
+    @date = date
+  end
+  
+end
---
+++ test/potluck_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/dish'
+require './lib/potluck'
+
+describe Potluck do
+
+  it "exists" do
+    potluck = Potluck.new("7-13-18")
+
+    expect(potluck).to be_an_instance_of(Potluck)
+  end
+end
